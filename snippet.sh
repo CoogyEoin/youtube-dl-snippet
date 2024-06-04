732f28821121f50b9e554ad3df18faddc685a6bd
BiasedRiot <biased@eoincoogan.com>
2024-06-04 14:27:04 +0100

    Revert "Fixing description"
    This reverts commit c4b24131e8f33cbf57319a47fa94e88b2b107f00.

--- snippet.sh
+++ snippet.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Usage: snippet.sh <Youtube URL> <Start time> <duration>
+# Usage: snippet.sh <Youtube URL> <Start time> <end time>
 # Timestamp must be in 00:00:00.00 format
 
 download_url=$(yt-dlp -f 'best' -g $1)
 
+<<<<<<< HEAD
 echo $download_url
 ffmpeg -i "$download_url" -ss $2 -t $3 -c copy "out.mp4"
+=======
+ffmpeg -ss $2 -i $download_url -t $3 -c copy out.mp4 
+>>>>>>> parent of c4b2413 (Fixing description)
