23be04cb937b55c6ae0392de55a009fbd2171eca
Eoin Coogan <professional@eoincoogan.com>
2024-09-25 11:55:47 +0100

    Fixing download bug

--- snippet.sh
+++ snippet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Usage: snippet.sh <Youtube URL> <Start time> <end time>
+# Usage: snippet.sh <Youtube URL> <Start time> <duration time>
 # Timestamp must be in 00:00:00.00 format
 
 download_url=$(yt-dlp -f 'best' -g $1)
 
-echo $download_url
-ffmpeg -i "$download_url" -ss $2 -t $3 -c copy "out.mp4"
+url_array=($download_url)
+ffmpeg -i "${url_array[0]}" -ss $2 -t $3 -c copy "out.mp4"
 
